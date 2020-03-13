602b38e06e8157dac65e07f867528b410ef525c8
Quentin Brunet <hello@quentinbrunet.com>
2020-03-13 17:25:46 +0100

    Hotfix winner in smart contract

--- contracts/CryptoTraderInternal.sol
+++ contracts/CryptoTraderInternal.sol
@@ -14,13 +14,17 @@ contract CryptoTraderInternal is CryptoTraderBase {
         address[] memory traders = competitionToTraders[currentCompetition];
 
         for (uint i = 0; i < traders.length; i++) {
-            competitionToTraderToCurrencyToBalance[currentCompetition][traders[i]][realCurrency] = 1000;
+            competitionToTraderToCurrencyToBalance[currentCompetition][traders[i]][virtualCurrency] = 1000;
         }
     }
 
     /// @notice Find the competition winner (the one who has the bigger balance) and return it.
-    /// @return address payable (winner)
-    function _getWinner(address[] memory traders, uint[] memory balances) internal pure returns (address payable) {
+    /// @return address (winner)
+    function _getWinner(address[] memory traders, uint[] memory balances) internal pure returns (address) {
+        if (0 == balances[0]) {
+            return address(0);
+        }
+
         uint maxBalance = balances[0];
         uint indexWinner = 0;
 
@@ -31,9 +35,7 @@ contract CryptoTraderInternal is CryptoTraderBase {
             }
         }
 
-        address winner = traders[indexWinner];
-        // cast to address payable
-        return address(uint160(winner));
+        return traders[indexWinner];
     }
 
     /// @notice Calculate the total balance of a trader in virtual coin.
@@ -41,5 +43,23 @@ contract CryptoTraderInternal is CryptoTraderBase {
     function _getTotalBalance(uint _competitionId, address _trader) internal view returns (uint) {
         return competitionToTraderToCurrencyToBalance[_competitionId][_trader][virtualCurrency] +
             _getVirtualCurrencyAmount(competitionToTraderToCurrencyToBalance[_competitionId][_trader][realCurrency]);
+    }
+
+    /// @notice Close current competition. Start the next competition straight after.
+    function _closeCompetition() internal {
+        address[] memory traders = competitionToTraders[currentCompetition];
+        uint[] memory balances = new uint[](traders.length);
+
+
+        for (uint i = 0; i < traders.length; i++) {
+            balances[i] = _getTotalBalance(currentCompetition, traders[i]);
+        }
+
+        // address winner = _getWinner(traders, balances);
+        // address(uint160(winner))
+        // winner.transfer(address(uint160(address(this))).balance);
+        // emit CloseCompetition(winner, traders, balances);
+
+        _restartCompetition();
     }
 }
