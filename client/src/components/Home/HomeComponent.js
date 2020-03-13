d30e24d7fb431634046a32db7ffd42997c0fa685
Léo <leo.legron@gmail.com>
2020-03-13 16:53:04 +0100

    join/leave competition setup

--- client/src/components/Home/HomeComponent.js
+++ client/src/components/Home/HomeComponent.js
@@ -1,60 +1,89 @@
 import React, { Component } from "react";
-import { Card, CardContent, Typography } from "@material-ui/core";
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import { green } from "@material-ui/core/colors";
-
-const useStyles = makeStyles(theme => ({
-    participate:{
-        color: green
-    }
-}));
-
-// const classes = useStyles();
-// const theme = useTheme();
-
+import { Card, CardContent, Typography, Button } from "@material-ui/core";
 
 export default class HomeComponent extends Component {
 
     constructor(props){
         super(props);
         this.state = {
-            n0Competition: '',
-            n0CompetitionTraders: '',
+            competitionToTraders: [],
+            currentCompetitionId: ''
         }
     }
 
     setData = async (contract) => {
-        const n0Competition = await contract.methods.currentCompetition().call()
-        const n0CompetitionTraders = await contract.methods.getTraders(n0Competition).call()
-        console.log(n0Competition)
-        console.log(n0CompetitionTraders)
+        let competitions = [];
+
+        let currentCompetitionId = Number(await contract.methods.currentCompetition().call())
+
+        competitions.push(currentCompetitionId)
+        competitions.push(currentCompetitionId+1)
+        competitions.push(currentCompetitionId+2)
+        competitions.push(currentCompetitionId+3)
+
+        const promises = competitions.map(async competitionId => {
+            return {
+                competitionId: competitionId,
+                traders: await contract.methods.getTraders(competitionId).call()
+            }
+        })
+
+        const competitionToTraders = await Promise.all(promises)
+
         this.setState({
-            n0Competition: n0Competition,
-            n0CompetitionTraders: n0CompetitionTraders
+            competitionToTraders: competitionToTraders,
+            currentCompetitionId: currentCompetitionId
         })
     }
 
     componentDidMount(){
+        console.log(this.props.contract.methods)
         this.setData(this.props.contract)
+    }
+
+    joinCompetition = async (id) => {
+        const res = await this.props.contract.methods.joinCompetition(id).send({from: this.props.accounts[0], value: this.props.web3.utils.toWei('0.001')})
+        .then(res => {
+            this.setData()
+        })
+        .catch(console.error)
+    }
+
+    leaveCompetition = async (id) => {
+        this.props.contract.methods.leaveCompetition(id).send({from: this.props.accounts[0], value: this.props.web3.utils.toWei('0.001')})
+        .then(res => {
+            this.setData()
+        })
+        .catch(console.error)
+    }
+
+    getCard = () => {
+        let cards = []
+
+        this.state.competitionToTraders.forEach(item => {
+            const participate = item.traders.includes(this.props.accounts[0])
+            const canParticipate = item.competitionId !== this.state.currentCompetitionId
+            cards.push(
+                <Card style={ item.traders.includes(this.props.accounts[0]) ? { backgroundColor:'green', marginTop: '15px'} : {backgroundColor:'red', marginTop: '15px'}} 
+                    key={item.competitionId}>
+                    <CardContent>
+                        <Typography>
+                            Competition {item.competitionId}
+                        </Typography>
+                        <Button disabled={participate || !canParticipate} onClick={() => {this.joinCompetition(item.competitionId)}}>Join</Button>
+                        <Button disabled={!participate} onClick={() => {this.leaveCompetition(item.competitionId)}}>Leave</Button>
+                    </CardContent>
+                </Card>
+            )
+        })
+
+        return cards
     }
 
     render(){
         return(
-            <div>
-                <Card className={useStyles.participate}>
-                    <CardContent>
-                        <Typography>
-                            Current competition
-                        </Typography>
-                    </CardContent>
-                </Card>
-                <Card >
-                    <CardContent>
-                        <Typography>
-                            S+1 competition
-                        </Typography>
-                    </CardContent>
-                </Card>
+            <div style={{paddingTop: '100px'}}>
+               { this.getCard() }
             </div>
         )
     }
