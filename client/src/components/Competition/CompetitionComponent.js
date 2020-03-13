8629e7c4cc75216a76b5781e5e901c733fe56194
Quentin Brunet <hello@quentinbrunet.com>
2020-03-13 18:47:34 +0100

    Add competitors balances

--- client/src/components/Competition/CompetitionComponent.js
+++ client/src/components/Competition/CompetitionComponent.js
@@ -6,17 +6,36 @@ export default class CompetitionComponent extends Component {
     constructor(props){
         super(props);
         this.state = {
-            traders: []
-        }
+            traders: [],
+            traderBalances: {},
+        };
     }
 
     setData = async (contract) => {
         let currentCompetitionId = Number(await contract.methods.currentCompetition().call())
         let traders = await contract.methods.getTraders(currentCompetitionId).call()
 
+        traders.forEach(trader => {
+            this.getBalance(trader);
+        });
+
         this.setState({
             traders: traders
-        })
+        });
+    }
+
+    getBalance = async (trader) => {
+        const { contract } = this.props;
+
+        let currentCompetitionId = Number(await contract.methods.currentCompetition().call())
+        let balances = await contract.methods.getBalances(currentCompetitionId, trader).call();
+
+        this.setState({
+            traderBalances: {
+                ...this.state.traderBalances,
+                [trader]: `${balances[1]/Math.pow(10,18)} ETH / ${balances[0]} VT`,
+            },
+        });
     }
 
     componentDidMount(){
@@ -24,12 +43,14 @@ export default class CompetitionComponent extends Component {
     }
 
     getTraderList = () => {
-        return this.state.traders.map(trader => {
+        const { traders, traderBalances } = this.state;
+
+        return traders.map(trader => {
             return (
                 <Card key={trader}>
                     <CardContent>
                         <Typography>
-                            { trader }
+                            { trader } || { traderBalances[trader] ? traderBalances[trader] : '-' }
                         </Typography>
                     </CardContent>
                 </Card>
@@ -42,5 +63,4 @@ export default class CompetitionComponent extends Component {
          <div>{ this.getTraderList() }</div>
         )
     }
-
 }
