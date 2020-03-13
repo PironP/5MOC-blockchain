b5559820549e94c573e9efd2bf9f3c3a1e5c61f1
Quentin Brunet <hello@quentinbrunet.com>
2020-03-13 11:07:33 +0100

    Integrate price feeder OSM + Deploy to Infura

---
+++ contracts/Helper/PriceHelper.sol
@@ -0,0 +1,16 @@
+pragma solidity >=0.4.21 <0.7.0;
+
+import "../OSM/OSM.sol";
+
+contract PriceHelper {
+    address OSMKovan = 0x9FfFE440258B79c5d6604001674A4722FfC0f7Bc;
+    address OSMMainNet = 0x81FE72B5A8d1A857d176C3E7d5Bd2679A9B85763;
+
+    OSM osmInstance = OSM(OSMKovan);
+
+    /// @notice Allow anyone to get real currency current price in Ether.
+    /// @return uint
+    function getPrice() public view returns (uint) {
+        return uint(osmInstance.read()) ;
+    }
+}
