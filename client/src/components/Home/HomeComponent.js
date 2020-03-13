2a9ef58c422f8047e6a595cfc70d26cce1c18071
Léo <leo.legron@gmail.com>
2020-03-13 15:20:11 +0100

    Working shit

--- client/src/components/Home/HomeComponent.js
+++ client/src/components/Home/HomeComponent.js
@@ -1,27 +1,61 @@
 import React, { Component } from "react";
+import { Card, CardContent, Typography } from "@material-ui/core";
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { green } from "@material-ui/core/colors";
+
+const useStyles = makeStyles(theme => ({
+    participate:{
+        color: green
+    }
+}));
+
+// const classes = useStyles();
+// const theme = useTheme();
+  
 
 export default class HomeComponent extends Component {
 
     constructor(props){
         super(props);
         this.state = {
-            price: ''
+            n0Competition: '',
+            n0CompetitionTraders: '',
         }
     }
 
-    setPrice = async (contract) => {
-        console.log(contract)
-        const price = await contract.methods.getPrice().call()
-        this.setState({price: price})
+    setData = async (contract) => {
+        const n0Competition = await contract.methods.currentCompetition().call()
+        const n0CompetitionTraders = await contract.methods.getTraders(n0Competition).call()
+        console.log(n0Competition)
+        console.log(n0CompetitionTraders)
+        this.setState({
+            n0Competition: n0Competition,
+            n0CompetitionTraders: n0CompetitionTraders
+        })
     }
 
     componentDidMount(){
-        this.setPrice(this.props.contract)
+        this.setData(this.props.contract)
     }
 
     render(){
         return(
-            <div>{this.state.price}</div>
+            <div>
+                <Card className={useStyles.participate}>
+                    <CardContent>
+                        <Typography>
+                            Current competition
+                        </Typography>
+                    </CardContent>
+                </Card>
+                <Card >
+                    <CardContent>
+                        <Typography>
+                            S+1 competition
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </div>
         )
     }
 }
--- client/src/components/Trades/TradesComponent.js
+++ client/src/components/Trades/TradesComponent.js
@@ -1,6 +1,31 @@
-import { Component } from "react";
+import React, { Component } from "react";
 
 
 export default class TradesComponent extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            currentCompetition: ''
+        }
+    }
+
+    setData = async (contract) => {
+        const currentCompetition = await contract.methods.currentCompetition().call()
+        const currentTraders = await contract.methods.getParticipants().call()
+        console.log(currentCompetition)
+        console.log(currentTraders)
+        this.setState({currentCompetition: currentCompetition})
+    }
+
+    componentDidMount(){
+        this.setData(this.props.contract)
+    }
+
+    render(){
+        return(
+            <div></div>
+        )
+    }
 
 }
